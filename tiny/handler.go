3a2d6377299295addb576ed224c4dc70a4ebe246
qida <sunqida@foxmail.com>
2020-05-19 19:12:58 +0800

    improve

--- tiny/handler.go
+++ tiny/handler.go
@@ -1,13 +1,16 @@
 package tiny
 
 import (
-	"net/http"
+	"bytes"
 	"encoding/base64"
 	"io/ioutil"
-	"bytes"
 	"log"
+	"net/http"
 	"path/filepath"
 	"strings"
+
+	"github.com/qida/go/logs"
+	"github.com/qida/go/nets"
 )
 
 const TinyApiHost = "https://api.tinify.com"
@@ -34,7 +37,7 @@ func (tinyhandler *TinyHandler) getAuthorCode(apikey string) string {
 
 /**
 上传图片
- */
+*/
 func (tinyhandler *TinyHandler) UploadFile(imgfilepath string) (error, string) {
 	apiurl := TinyApiHost + "/shrink"
 	imgBytes, err := ioutil.ReadFile(imgfilepath)
@@ -52,20 +55,20 @@ func (tinyhandler *TinyHandler) UploadFile(imgfilepath string) (error, string) {
 		return err, ""
 	}
 	imgUrl := res.Header.Get("Location")
-	log.Println("img url", imgUrl)
-	resBytes, err := ioutil.ReadAll(res.Body)
+	// log.Println("img url", imgUrl)
+	_, err = ioutil.ReadAll(res.Body)
 	if err != nil {
 		return err, imgUrl
 	}
-	log.Println("res", string(resBytes))
+	log.Printf("处理中 图片：[%s]\r\n", imgfilepath)
+	// logs.Send2Dingf(logs.Rb监控, "图片压缩：%s", string(resBytes))
 	return nil, imgUrl
 }
 
 /**
 下载图片
- */
+*/
 func (tinyhandler *TinyHandler) DownloadImg(imgUrl string, outFilePath string) error {
-
 	req, err := http.NewRequest("GET", imgUrl, nil)
 	if err != nil {
 		return err
@@ -85,7 +88,7 @@ func (tinyhandler *TinyHandler) DownloadImg(imgUrl string, outFilePath string) e
 
 /**
 压缩单张图片
- */
+*/
 func (tinyHandler *TinyHandler) CompressImageFile(imgFile string, outFile string) {
 	if outFile == "" {
 		fileName := filepath.Base(imgFile)
@@ -93,37 +96,53 @@ func (tinyHandler *TinyHandler) CompressImageFile(imgFile string, outFile string
 	}
 	err, imgUrl := tinyHandler.UploadFile(imgFile)
 	if err != nil {
-		log.Println("上传图片错误", err, imgFile)
+		logs.Send2Dingf(logs.Rb错误, "图片压缩 上传图片错误：[%s] %s", imgFile, err.Error())
 	}
 	if imgUrl == "" {
-		log.Println("上传图片错误_图片url为空", imgFile)
+		logs.Send2Dingf(logs.Rb错误, "图片压缩 图片错误_图片url为空 [%s]", imgFile)
 		return
 	}
 	err = tinyHandler.DownloadImg(imgUrl, outFile)
 	if err != nil {
-		log.Println("下载图片失败", imgFile, err)
+		logs.Send2Dingf(logs.Rb错误, "图片压缩 下载图片失败：[%s] %s", imgFile, err.Error())
 		return
 	}
-	log.Println("下载图片成功", imgFile, outFile)
+	log.Printf("压缩完 图片：[%s]\r\n", imgFile)
+	log.Println(strings.Repeat("*", NumRepeat))
+	logs.Send2Dingf(logs.Rb监控, "图片压缩 下载成功：[%s]", imgFile)
 }
+
+var NumRepeat = 30
 
 /**
 压缩文件夹内的所有图片
- */
+*/
 func (tinyHandler *TinyHandler) CompressAllImages(imgsDir string, outDir string) error {
 	fileinfoList, err := ioutil.ReadDir(imgsDir)
 	if err != nil {
 		return err
 	}
-	for index, itemFile := range fileinfoList {
+	log.Printf("只支持 jpg png 格式\r\n\r\n")
+	log.Printf("如长时间没有反应可按回车键激活任务\r\n\r\n")
+	log.Println("qida v1.0")
+	log.Println(strings.Repeat("=", NumRepeat))
+	var num int
+	for _, itemFile := range fileinfoList {
 		if itemFile.IsDir() || itemFile.Size() <= 0 {
 			continue
 		}
 		if !strings.Contains(itemFile.Name(), "png") && !strings.Contains(itemFile.Name(), "jpg") {
 			continue
 		}
-		log.Println("压缩图片", index, itemFile.Name())
+		num++
+		log.Printf("准备中 图片：[%s]\r\n", itemFile.Name())
 		tinyHandler.CompressImageFile(filepath.Join(imgsDir, itemFile.Name()), outDir)
+	}
+	log.Printf("已完成 图片：%d 个\r\n", num)
+	log.Println(strings.Repeat("=", NumRepeat))
+	ip, err := nets.ExternalIP()
+	if err == nil {
+		logs.Send2Dingf(logs.Rb监控, "图片压缩 Num: %d IP:%s", num, ip.String())
 	}
 	return nil
 }
